d98073f8dd22674260efbb00fa5b8c2afa400df5
Kirill Rasuli <rasulikv@gmail.com>
2020-10-22 20:19:38 +0300

    Send back a random question

--- bot.py
+++ bot.py
@@ -2,30 +2,36 @@ from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram
 import os
+import random
+from functools import partial
 
 
 def unpack_questions():
     with open('1vs1200.txt', 'r', encoding='koi8-r') as file:
         quiz_content = file.read()
-
     questions_from_file = quiz_content.split('\n\n')
-
     questions = dict()
     for index, question in enumerate(questions_from_file):
         question = question.lstrip()
         if question.startswith('Вопрос'):
             questions[question] = questions_from_file[index + 1]
+    return questions
 
 
-def echo(update, context):
-    custom_keyboard = [['Новый вопрос', 'Сдаться'],['Мой счет']]
+def echo(update, context, questions):
+    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    update.message.reply_text(update.message.text, reply_markup=reply_markup)
+    if update.message.text == 'Новый вопрос':
+        answer = random.choice(list(questions.keys()))
+        update.message.reply_text(answer, reply_markup=reply_markup)
 
 
 def run_bot(token):
+    questions = unpack_questions()
     updater = Updater(token, use_context=True)
-    updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    updater.dispatcher.add_handler(
+        MessageHandler(Filters.text, partial(echo, questions=questions))
+    )
     updater.start_polling()
     updater.idle()
 
@@ -33,7 +39,6 @@ def run_bot(token):
 def main():
     load_dotenv()
     telegram_token = os.getenv('TELEGRAM_TOKEN')
-    # unpack_questions()
     run_bot(telegram_token)
 
 
