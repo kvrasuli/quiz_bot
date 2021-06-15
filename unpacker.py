b698318a43d6651e010c6c93186a928831798fe4
Kirill Rasuli <rasulikv@gmail.com>
2021-06-15 11:12:39 +0300

    Fix var name

--- unpacker.py
+++ unpacker.py
@@ -5,11 +5,11 @@ def unpack_questions(path_to_questions):
     with open(path_to_questions, 'r', encoding='koi8-r') as file:
         quiz_content = file.read()
     questions_from_file = quiz_content.split('\n\n')
-    questions = dict()
+    unpacked_questions = dict()
     for index, question in enumerate(questions_from_file):
         question = question.lstrip()
         if question.startswith('Вопрос'):
             answer = questions_from_file[index + 1].lstrip('Ответ:\n')
-            questions[question] = answer
+            unpacked_questions[question] = answer
     logger.info('Questions have been unpacked!')
-    return questions
+    return unpacked_questions
