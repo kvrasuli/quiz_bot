ac10e4111754ba597ee977a7ac15fdc9c83fb429
Kirill Rasuli <rasulikv@gmail.com>
2021-06-15 14:14:58 +0300

    Rework unpacker

--- unpacker.py
+++ unpacker.py
@@ -1,11 +1,24 @@
+from pathlib import Path
+import logging
+
+logger = logging.getLogger(__file__)
+
+
 def unpack_questions(path_to_questions):
-    with open(path_to_questions, 'r', encoding='koi8-r') as file:
-        quiz_content = file.read()
-    questions_from_file = quiz_content.split('\n\n')
     unpacked_questions = dict()
-    for index, question in enumerate(questions_from_file):
-        question = question.lstrip()
-        if question.startswith('Вопрос'):
-            answer = questions_from_file[index + 1].lstrip('Ответ:\n')
-            unpacked_questions[question] = answer
+    for filename in Path(path_to_questions).iterdir():
+        with open(filename, 'r', encoding='koi8-r') as file:
+            quiz_content = file.read()
+        questions_from_file = quiz_content.split('\n\n') 
+        for index, question in enumerate(questions_from_file):
+            question = question.lstrip()
+            if question.startswith('Вопрос'):
+                answer = questions_from_file[index + 1].lstrip('Ответ:\n')
+                unpacked_questions[question] = answer
+    logger.info(f'Total {len(unpacked_questions)} questions unpacked!')
     return unpacked_questions
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    unpack_questions('questions')
